0434d41209fa10f1c90630a9e7f996f10d652a17
Bahrudin <bahrudin.trbalic10@hotmail.com>
2024-03-28 12:51:01 -0700

    Update reconstruction_tools.py

--- tools/reconstruction_tools.py
+++ tools/reconstruction_tools.py
@@ -4,11 +4,7 @@ import awkward as ak
 from tqdm import tqdm
 import pandas as pd
 
-# events with [1, 2] hits will have a seperate treatement
-# events with [3, 4, 5, 6, 7] hits will be treated with CKD, 
-# events with more than 8 hits will be treated with probabilistic methods
 
-# first we will calculate the CKD for the events with [3, 4, 5, 6] hits
 # the result of the reconstruction will have parameters inherited from 
 # the truth, such as the total energy, incoming angle, missing ennergy stats
 # ID and so on
@@ -82,8 +78,8 @@ def reconstruct(events,
         outside_mask = ((np.sum(events_to_reconstruct['energy'], axis=1) 
                          + events_to_reconstruct['calorimeter_energy']) > MIN_ENERGY)  
 
-    data = {}
     CKD_DEPTH = 2
+    data = {}
     for hit_len in LEN_OF_CKD_HITS:
         data[hit_len] = {}
         hit_len_mask = ak.num(events_to_reconstruct['energy']) == hit_len
@@ -91,33 +87,36 @@ def reconstruct(events,
         calorimeter_energy = events_to_reconstruct['calorimeter_energy'][hit_len_mask&outside_mask]
         positions = events_to_reconstruct['r'][hit_len_mask&outside_mask]
         #recoil = events_to_reconstruct['s_secondary'][hit_len_mask&outside_mask]
-
+    
         hit_len_prime = hit_len
         PERMUTATIONS = list(permutations(range(hit_len)))
         if hit_len > CKD_DEPTH + 2:
             hit_len_prime = CKD_DEPTH + 3
             PERMUTATIONS = list(permutations(range(hit_len),
-                                            hit_len_prime))
+                                             hit_len_prime))
             
+        PERMUTATIONS = PERMUTATIONS[:300]
+        #TODO: remove this for accurate permutation search
+    
         e_out_error        = np.zeros((len(energies), len(PERMUTATIONS)))
         e_out_error_prime  = np.zeros((len(energies), len(PERMUTATIONS)))
         total_energy = (np.sum(energies, axis=1) 
                         + calorimeter_energy)
-
+    
         for p,permutation in tqdm(enumerate(PERMUTATIONS), desc=f"Scatter {hit_len}"):
             cumulative_energies = np.cumsum(energies[:,list(permutation)],axis=1)
             for i in range(hit_len_prime - 2):
                 r1_r2 = positions[:,:,permutation[i+1]] - positions[:,:,permutation[i]]
                 r2_r3 = positions[:,:,permutation[i+2]] - positions[:,:,permutation[i+1]]
                 geometric_cosine = geometric_cos(r1_r2, r2_r3)
-
+    
                 E_in = total_energy - cumulative_energies[:,i]
                 E_ou = E_in - energies[:,permutation[i+1]]
-
+    
                 kn = klein_nishina(E_in, np.arccos(geometric_cosine))
-
+    
                 eouterr = eout_scatter_error(E_in, E_ou, geometric_cosine)
-                e_out_error[:,p] += np.array(eouterr) - np.array(np.log(kn))
+                e_out_error[:,p] += np.array(eouterr) - np.array(np.log(kn))/hit_len
                 
                 # calculate the expected energy and make inferences about
                 # the order of the hits (calorimeter first?)
@@ -131,10 +130,10 @@ def reconstruct(events,
                 
                 if hit_len > 4: # here I suscpect that some hits are lost
                     eouterr_prime = eout_scatter_error(E_in[iem] + e_surplus[iem], 
-                                                    E_ou[iem] + e_surplus[iem], 
+                                                       E_ou[iem] + e_surplus[iem], 
                                                             geometric_cosine[iem])
                     e_out_error_prime[:,p][iem] += np.array(eouterr_prime)
-
+    
                 # Here I suspect that calorimeter came in first or second
                 eouterr_second = eout_scatter_error(E_in[sec] - calorimeter_energy[sec], 
                                                     E_ou[sec] - calorimeter_energy[sec], 
@@ -147,22 +146,22 @@ def reconstruct(events,
         e_out_error_prime[e_out_error_prime==0] = 1e12
         prime_better = e_out_error > e_out_error_prime
         e_out_error[prime_better] = e_out_error_prime[prime_better]
-
+    
         best_e_out_order = np.argmin(e_out_error, axis=1)
         best_e_out       = np.min(e_out_error, axis=1)**0.5 
-
-        data[hit_len]['best_e_out_order'] = best_e_out_order
-        data[hit_len]['e_out_CKD']        = best_e_out
-        data[hit_len]['energy_from_sum']  = total_energy  
-
+    
         klein_nishina_sums = np.ones(len(best_e_out_order))
         min_distances      = np.ones(len(best_e_out_order))
+        first_arm_len      = np.zeros(len(best_e_out_order))
         calculated_energy  = np.zeros(len(best_e_out_order))
         first_hit_compton  = np.zeros(len(best_e_out_order))
+        first_hit_comptonC = np.zeros(len(best_e_out_order))
         first_hit_dot_ang  = np.zeros(len(best_e_out_order))
         truth_angle12      = np.zeros(len(best_e_out_order))
-
-
+        psi_angle = np.zeros(len(best_e_out_order))
+        xsi_angle = np.zeros(len(best_e_out_order))
+    
+    
         # now calculate the relevant stats given the order
         for p,permutation in enumerate(PERMUTATIONS):
             u_mask = p == best_e_out_order
@@ -171,67 +170,85 @@ def reconstruct(events,
             positions_m = positions[u_mask]
             energies_m  = energies[u_mask]
             cumulative_energies = np.cumsum(energies_m[:,list(permutation)],axis=1)
-
-
+    
+    
         
             r1_r2 = positions_m[:,:,permutation[1]] - positions_m[:,:,permutation[0]]
             r2_r3 = positions_m[:,:,permutation[2]] - positions_m[:,:,permutation[1]]
             geometric_cosine = geometric_cos(r1_r2, r2_r3)
             
             E_in = total_energy[u_mask] - cumulative_energies[:,0]
-
+    
             kn = klein_nishina(E_in, np.arccos(geometric_cosine))
             klein_nishina_sums[u_mask] *= np.array(kn)
-
+    
             e2 = energies_m[:,permutation[1]]
             e3 = e_3_formula(e2, geometric_cosine)
-            calculated_in = e3+ e2 + energies_m[:,permutation[0]]
+            calculated_in = e3 + e2 + energies_m[:,permutation[0]]
             calculated_energy[u_mask] = np.array(calculated_in)
-
-            min_arm_len = np.minimum(np.linalg.norm(r1_r2, axis=1), 
-                                        np.linalg.norm(r2_r3, axis=1))
+    
+    
+            first_arm_len[u_mask] = np.linalg.norm(r1_r2, axis=1)
+            min_arm_len = np.minimum(first_arm_len[u_mask], 
+                                    np.linalg.norm(r2_r3, axis=1))
             min_distances[u_mask] = min_arm_len
-
+    
             theta = calculate_compton_angle(total_energy[u_mask], E_in)
             first_hit_compton[u_mask] = theta
-            first_hit_dot_ang[u_mask] = np.arccos(geometric_cos(r1_r2, 
-                                                                np.array([IN_VECTOR])))
+    
+            theta_c = calculate_compton_angle(calculated_in, 
+                                              calculated_in - energies_m[:,permutation[0]])
+            first_hit_comptonC[u_mask] = theta_c
+    
+            first_hit_dot_ang[u_mask] = np.arccos(geometric_cos(r1_r2, np.array([IN_VECTOR])))
+            psi_angle[u_mask] = np.arccos(geometric_cos(r1_r2, np.array([[1,0, 0]])))
+            xsi_angle[u_mask] = np.arccos(geometric_cos(r1_r2, np.array([[0,0,-1]])))
+    
             R1R2 = events.truth_hits.s_primary[hit_len_mask&outside_mask][u_mask][:,:,permutation[0]] 
             truth_angle12[u_mask] = np.arccos(geometric_cos(r1_r2, R1R2))*180/np.pi
                 
         first3_good = np.array(PERMUTATIONS)[best_e_out_order,:3]==[0,1,2]
-
+    
+        data[hit_len]['best_e_out_order'] = best_e_out_order
+        data[hit_len]['e_out_CKD']        = best_e_out
+        data[hit_len]['energy_from_sum']  = total_energy  
+    
         data[hit_len]['min_hit_distance']  = min_distances
+        data[hit_len]['first_arm_len']     = first_arm_len
         data[hit_len]['kn_probability']    = klein_nishina_sums
-        data[hit_len]['calculated_energy'] = calculated_energy/total_energy - 1
+        data[hit_len]['calculated_energy'] = calculated_energy
+        data[hit_len]['calc_to_sum_ene']   = calculated_energy/total_energy - 1
         data[hit_len]['num_of_hits']       = hit_len*np.ones(len(best_e_out_order))
-        data[hit_len]['first_scatter_angle'] = first_hit_compton
-
-        data[hit_len]['ARM'] = (first_hit_compton - first_hit_dot_ang) * 180/np.pi
-
+    
+        data[hit_len]['compton_angle']     = first_hit_compton
+        data[hit_len]['psi_angle']         = psi_angle
+        data[hit_len]['xsi_angle']         = xsi_angle
+    
+        data[hit_len]['ARM']  = (first_hit_compton  - first_hit_dot_ang) * 180/np.pi
+        data[hit_len]['ARMc'] = (first_hit_comptonC - first_hit_dot_ang) * 180/np.pi
+    
         data[hit_len]['truth_lost_in_passive'] = events.truth.passive_energy[hit_len_mask&outside_mask] > 15
         data[hit_len]['truth_escaped_energy']  = events.truth.escaped_energy[hit_len_mask&outside_mask] > 15
         data[hit_len]['truth_correct_order']   = first3_good.all(axis=1)
         data[hit_len]['truth_angle12']         = truth_angle12
         data[hit_len]['truth_calorimeter_first3'] = events.truth.calorimeter_in_first_3[hit_len_mask&outside_mask]
-
+    
         this = data[hit_len]
-        bad_events = (this['truth_escaped_energy'] 
-                    | this['truth_calorimeter_first3'])
+        bad_events = ( this['truth_escaped_energy'] 
+                      | this['truth_calorimeter_first3'])
         
         correct_order = this['truth_correct_order']
-
+    
         print(f"-> {hit_len} hits:",end=' | ')
         #print percentage of good events
         print(f"Good events: {100*sum(~bad_events)/len(bad_events):.2f} %", end=' | ')
         print(f"Correct order: {100*sum(correct_order)/len(correct_order):.2f} %")
 
-
     dfs = [pd.DataFrame(data[k]) for k in sorted(data.keys())]
     df = pd.concat(dfs, axis=0, ignore_index=True)
 
     if len(filename) > 0:
-        df.to_pickle(f'{filename}_reconstructed.pkl')
+        df.to_pickle(f'recon_{filename}.pkl')
 
     return df
     
@@ -247,7 +264,7 @@ def train_classifier(df, filename='classifier.pkl', plot_confusion_matrix=True):
             'kn_probability', 
             'calculated_energy', 
             'num_of_hits', 
-            'first_scatter_angle',
+            'compton_angle',
             ]]
 
     y = df[['truth_escaped_energy', 'truth_correct_order', 'truth_calorimeter_first3']]
