7f891ad91c14cfe5056dd1c853bf880bc337601a
Bahrudin <bahrudin.trbalic10@hotmail.com>
2024-06-18 15:05:32 -0700

    Add files via upload

--- sensitivity_study/gamma_TPC_response.py
+++ sensitivity_study/gamma_TPC_response.py
@@ -1,18 +1,15 @@
 #%%
+# install all dependencies using pip
+# !pip install pandas numpy matplotlib seaborn joblib scipy h5py awkward tqdm
+# !pip install scikit-learn==1.3.0
+
+
 import os
 import sys
 import pickle
 import awkward as ak
 import numpy as np
 import subprocess
-import argparse
-
-# get sim_file_name from argparse
-parser = argparse.ArgumentParser(description='Input the name of the simulation file .sim')
-parser.add_argument('sim_file_name', type=str, help='The name of the simulation file .sim')
-parser.add_argument('study',         type=str, help='The study to be performed: Optimistic, Nominal, Pessimistic')
-parser.add_argument('N_hits_to_reconstruct', type=int, default=11 ,help='The number of hits to be reconstructed')
-args = parser.parse_args()
 
 tools_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(tools_dir)
@@ -21,26 +18,21 @@ sys.path.append('tools')
 import events_tools
 import params_tools
 
-
-sim_file_name         = args.sim_file_name
-STUDY                 = args.study
-N_hits_to_reconstruct = args.N_hits_to_reconstruct
+sim_file_name         = "FarFieldPointSource_10.000MeV_Cos1.0.inc1.id1.sim"
+STUDY                 = "Nominal"
+N_hits_to_reconstruct = 11
 
 # extract the energy and the angle (and time) from the file name
 try:
     in_energy = float(sim_file_name.split('_')[1].strip('MeV')) * 1000
     in_angle  = float(sim_file_name.split('_')[2].strip('Cos').replace('.inc1.id1.sim', ''))
-    print('Energy, Angle:', in_energy, in_angle)
-
 except:
     in_energy = 1000
     in_angle = 1.0
-    parts = sim_file_name.split('_')
-    in_time = float(parts[1])
-    min_energy = float(parts[3].strip("minEnergy").replace('.sim', ''))
-    print("Time:", in_time)
-    print("Min Energy:", min_energy)
+    in_time = float(sim_file_name.split('background_')[1].replace('.sim', ''))
+    print("Time", in_time) 
 
+print('Energy, Angle:', in_energy, in_angle)
 
 sim_file_name = sim_file_name.strip('.sim')
 
@@ -125,6 +117,9 @@ mask[len_mask] = R < R_max
 print("Masking events", len(truth), "to", sum(mask), 
       f"for R < {R_max:.3f} m. That's {sum(mask)/len(truth)*100:.2f}% of the events.")
 
+num_hits = truth[mask]['num_hits']
+hit_dist = np.bincount(num_hits, minlength=max(num_hits)+1)
+
 summary_dict = {
     "energy": in_energy,
     "angle": in_angle,
@@ -132,19 +127,16 @@ summary_dict = {
     "number_of_events_after_cuts": sum(mask),
     "study": STUDY,
     "r_max": R_max,
-    "N_hits_to_reconstruct": N_hits_to_reconstruct}
+    "N_hits_to_reconstruct": N_hits_to_reconstruct,
+    "hit_distribution": list(hit_dist)}
 
-with open(os.path.join(f"{sim_file_name.split(".inc")[0]}_summary.pickle"), 'wb') as f:
-    pickle.dump(summary_dict, f)
 
 events.truth = truth[mask]    
 events.truth_hits = hits[mask]
 
-# DETECTOR RESPONSE
 events.apply_detector_response()
 
 
-# RECONSTRUCTION
 in_vector = np.array([-np.sqrt(1-in_angle**2), 0, -in_angle])
 
 # a list starting with 3 and ending with N_hits_to_reconstruct
@@ -154,9 +146,12 @@ events.reconstruct_events(IN_VECTOR=in_vector,
                           LEN_OF_CKD_HITS = HIT_LIST)
 
 
+#%%
 
-# CLASSIFICATION
 # events.train_classifier_on_self()
+
 # events.classify_reconstructed_events(save_name=sim_file_name)
 
 
+
+# %%
