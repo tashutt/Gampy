f5958069607c54bedb138da784306c2ae7f03828
Bahrudin <bahrudin.trbalic10@hotmail.com>
2025-05-27 12:53:32 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gampy",
+    version="0.1.0",
+    packages=find_packages(),
+    description="GammaTPC simulation and analysis tools",
+)
